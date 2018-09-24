1b2170d4f1455585640e56315388ced42dc5deaa
eswaribala <Parameswaribala@gmail.com>
2018-09-24 13:51:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',function()
+{
+   // alert("loaded...");
+
+   var aRef=document.getElementById("register");
+
+   aRef.addEventListener('click',function()
+   {
+       window.open('register.html','regForm','width=800 height=500 left=200');
+   })
+
+})
